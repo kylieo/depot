c09f52c43193b4724e430192e23ef0ca6d39a73a
Kylie O'Brien-Pratt <Kylie@Kylies-MacBook-Air.local>
2015-09-25 15:39:39 +1000

    Depot scaffold

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :products
+  # == Route Map
+#
+#              Prefix Verb   URI Pattern                         Controller#Action
+#            products GET    /products(.:format)                 products#index
+#                     POST   /products(.:format)                 products#create
+#         new_product GET    /products/new(.:format)             products#new
+#        edit_product GET    /products/:id/edit(.:format)        products#edit
+#             product GET    /products/:id(.:format)             products#show
+#                     PATCH  /products/:id(.:format)             products#update
+#                     PUT    /products/:id(.:format)             products#update
+#                     DELETE /products/:id(.:format)             products#destroy
+end
